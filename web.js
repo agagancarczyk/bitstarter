af16e62447f6a46061e988cda3ddc5e8bf251514
John Smith <example@stanford.edu>
2013-07-10 20:46:50 +0000

    dupa

--- web.js
+++ web.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html').toString());
+  response.send(fs.readFileSync("index.html").toString());
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
